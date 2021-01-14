506eb11dde1b672bb8b677dd89abe89239869d82
darryncampbell <darryncampbell@hotmail.com>
2021-01-14 10:57:05 +0000

    Initial check-in

---
+++ app/src/main/java/com/darryncampbell/playstore_onlyzebradevices/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.darryncampbell.playstore_onlyzebradevices
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
